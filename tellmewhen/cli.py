b7fa335f9fca65db3f7d46d05642e971f758ad82
Joseph Lansdowne <J49137@gmail.com>
2016-08-29 15:13:45 +0100

    better error messages for invalid config

--- tellmewhen/cli.py
+++ tellmewhen/cli.py
@@ -14,42 +14,67 @@ all_option_defns = list(option.option_defns.__dict__.values())
 VERSION = '0.1.0-next'
 
 
-def check (valid):
+class ConfigError (ValueError):
+    def __init__ (self, *msg):
+        self.msg = msg
+        ValueError.__init__(self, ' '.join(map(str, msg)))
+
+
+def check (valid, source, *msg):
     if not valid:
-        raise ValueError('invalid config')
+        raise ConfigError(
+            'invalid config: {}: {}'.format(source, ' '.join(map(str, msg))))
 
 
-def validate_config_triggers (triggers):
-    check(isinstance(triggers, list))
-    for t in triggers:
-        check(isinstance(t, dict))
-        check(set(t.keys()) == set(['offset_time']))
+def check_obj (obj, source, names):
+    check(isinstance(obj, dict), source, 'expected an object')
+    check(
+        set(obj.keys()) == set(names), source,
+        'expected exactly properties: ' +
+            ', '.join('`{}`'.format(name) for name in names))
 
-        check(isinstance(t['offset_time'], (int, float)))
+
+def validate_config_triggers (triggers, source):
+    check(isinstance(triggers, list), source, 'expected a list')
+    for i, t in enumerate(triggers):
+        t_source = '{}[{}]'.format(source, i)
+        check_obj(t, t_source, ['offset_time'])
+
+        check(isinstance(t['offset_time'], (int, float)),
+              t_source + '.offset_time',
+              'expected a number')
 
 
-def validate_config_events (events):
-    check(isinstance(events, list))
-    for e in events:
-        check(isinstance(e, dict))
-        check((set(e.keys()) ==
-                set(['name', 'command', 'separation_time', 'triggers'])))
+def validate_config_events (events, source):
+    check(isinstance(events, list), source, 'expected a list')
+    for i, e in enumerate(events):
+        e_source = '{}[{}]'.format(source, i)
+        check_obj(e, e_source,
+                  ['name', 'command', 'separation_time', 'triggers'])
 
-        check(isinstance(e['name'], str))
+        check(isinstance(e['name'], str),
+              e_source + '.name',
+              'expected a string')
         check((isinstance(e['command'], list) and len(e['command']) > 0 and
-                all(isinstance(w, str) for w in e['command'])))
+                all(isinstance(w, str) for w in e['command'])),
+              e_source + '.command',
+              'expected a non-empty list of strings')
         check((isinstance(e['separation_time'], (int, float)) and
-                e['separation_time'] > 0))
-        validate_config_triggers(e['triggers'])
+                e['separation_time'] > 0),
+              e_source + '.separation_time',
+              'expected a number')
+        validate_config_triggers(e['triggers'], e_source + '.triggers')
 
     assert(len(set(e['name'] for e in events)) == len(events))
 
 
-def read_config (file_obj):
-    config = json.load(file_obj)
+def read_config (file_obj, source):
+    try:
+        config = json.load(file_obj)
+    except json.JSONDecodeError as e:
+        check(False, source, 'invalid JSON:', e)
     file_obj.close()
-    check(isinstance(config, dict))
-    check(set(config.keys()) == set(['events']))
+    check_obj(config, source, ['events'])
     return config
 
 
@@ -91,18 +116,23 @@ the README file for details on the structure of this file
 
     args = p.parse_args()
 
-    config_events = sum([read_config(config_file_obj)['events']
-                         for config_file_obj in args.config], [])
-    validate_config_events(config_events)
-    event_defns = [
-        event.Event(e['name'], e['command'], e['separation_time'],
-                    [event.Trigger(t['offset_time']) for t in e['triggers']])
-        for e in config_events
-    ]
+    event_defns = []
+    try:
+        for config_file_obj in args.config:
+            path = config_file_obj.name
+            config_events = read_config(config_file_obj, path)['events']
+            validate_config_events(config_events, path + ': events')
+            for e in config_events:
+                event_defns.append(event.Event(
+                    e['name'], e['command'], e['separation_time'],
+                    [event.Trigger(t['offset_time']) for t in e['triggers']]))
+    except ConfigError as e:
+        p.error(*e.msg)
 
-    raw_options = {}
-    for o in all_option_defns:
-        raw_options[o.name] = getattr(args, opt_var_name(o))
-    options = option.Options(raw_options)
+    else:
+        raw_options = {}
+        for o in all_option_defns:
+            raw_options[o.name] = getattr(args, opt_var_name(o))
+        options = option.Options(raw_options)
 
-    return (event_defns, options)
+        return (event_defns, options)
